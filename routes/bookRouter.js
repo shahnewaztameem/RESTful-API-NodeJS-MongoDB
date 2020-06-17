4e9856bcdaf076629cc3500f786b54e398caea6f
shahnewaz tameem <shahnewaztamim@gmail.com>
2020-06-18 01:43:33 +0600

    Add PUT, PATCH

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,3 +1,5 @@
+/*  eslint-disable no-param-reassign  */
+
 const express = require('express');
 
 const bookRouter = express.Router();
@@ -20,14 +22,54 @@ function routes(Book) {
         return res.json(books);
       });
     });
-  bookRouter.route('/books/:bookId').get((req, res) => {
+  bookRouter.use('/books/:bookId', (req, res, next) => {
     Book.findById(req.params.bookId, (error, book) => {
       if (error) {
         return res.send(error);
       }
-      return res.json(book);
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
     });
   });
+
+  bookRouter.route('/books/:bookId')
+    .get((req, res) => res.json(req.book))
+    .put((req, res) => {
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      req.book.save((error) => {
+        if (error) {
+          return res.send(error);
+        }
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      //  eslint-disable-next-line no-underscore-dangle
+      if (req.body._id) {
+        //  eslint-disable-next-line no-underscore-dangle
+        delete req.body._id;
+      }
+
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+      req.book.save((error) => {
+        if (error) {
+          return res.send(error);
+        }
+        return res.json(book);
+      });
+    });
   return bookRouter;
 }
 module.exports = routes;
