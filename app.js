d03f198691d73f2713d6f613f71419028e50da20
shahnewaz tameem <shahnewaztamim@gmail.com>
2020-06-23 03:03:28 +0600

    Modified app

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.get('/', (req, res) => {
   res.send('Welcome to my API');
 });
 
-app.listen(port, () => {
+app.server = app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
+
+module.exports = app;
