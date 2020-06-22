be17c95b7819e8a1fe9e7de218b9f42a2ff33985
shahnewaz tameem <shahnewaztamim@gmail.com>
2020-06-23 03:04:14 +0600

    Add controller

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,8 +1,13 @@
 function booksController(Book) {
   function post(req, res) {
     const book = new Book(req.body);
+    if (!req.body.title) {
+      res.status(400);
+      return res.send('Title is required');
+    }
     book.save();
-    return res.status(201).json(book);
+    res.status(201);
+    return res.json(book);
   }
 
   function get(req, res) {
